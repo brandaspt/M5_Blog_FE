5a5dfb02b3aad84309592b0b2baba6be13db73bd
Tiago Brandao <tiagofpbrandao@gmail.com>
2021-06-30 17:43:11 +0200

    Adds pdf download on posts

--- src/views/blog/index.jsx
+++ src/views/blog/index.jsx
@@ -4,6 +4,7 @@ import { withRouter } from "react-router"
 import { Link } from "react-router-dom"
 import backend from "../../client"
 import BlogAuthor from "../../components/blog/blog-author"
+import { BACKEND_URL } from "../../const/env"
 import "./styles.css"
 class Blog extends Component {
   state = {
@@ -32,11 +33,14 @@ class Blog extends Component {
             <Image className="blog-details-cover" src={blog.cover} fluid />
             <h1 className="blog-details-title my-3">{blog.title}</h1>
             <div className="d-flex justify-content-start mb-3">
-              <Button as={Link} to={`/blog/${this.state.blog._id}/edit`} variant="dark" className="me-2 btn-sm">
+              <Button as={Link} to={`/blog/${this.state.blog._id}/edit`} variant="dark" className="btn-sm">
                 Edit
               </Button>
-              <Button variant="danger" className="btn-sm" onClick={this.handleDelete}>
+              <Button variant="danger" className="btn-sm mx-2" onClick={this.handleDelete}>
                 Delete
+              </Button>
+              <Button variant="info" href={`${BACKEND_URL}/blogPosts/${this.state.blog._id}/downloadPDF`} className="btn-sm" as="a">
+                Download PDF
               </Button>
             </div>
 
